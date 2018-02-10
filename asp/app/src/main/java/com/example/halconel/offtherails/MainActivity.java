ab871a0cb786521cac9bb35fa32e5ba275695aa3
Дмитрий Виноградов <di.vinogradov@gmail.com>
2018-02-11 02:45:53 +0300

    Перенес проект Android Studio в отдельный каталог. Добавил описание этого каталога в файл structure.md \app\src\main\res\layout\activity_main.xml - добавил пустое Activity для отображения приложения \asp\app\src\main\res\values\styles.xml - добавил стиль для полноэкранного режима \asp\app\src\main\AndroidManifest.xml - сменил тему на полноэкранную \asp\app\src\main\java\com\example\halconel\offtherails\MainActivity.java - залил экран цветом и вывел надпись "Это только начало". В коде добавил комментарии что бы можно было разобраться как я это сделал

---
+++ asp/app/src/main/java/com/example/halconel/offtherails/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.halconel.offtherails;
+
+        import android.app.Activity;
+        import android.content.Context;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.os.Bundle;
+        import android.view.View;
+
+public class MainActivity extends Activity {
+
+    private Paint mPaint; //Paint говорит нам как именно рисовать графические примитивы
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawView(this));
+    }
+
+    class DrawView extends View {
+
+        public DrawView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // Что бы приложение выглядело не так скучно, зальем его цветом
+            canvas.drawColor(Color.parseColor("#ffbb6e"));
+
+            mPaint = new Paint();
+            mPaint.setAntiAlias(true); //включить сглаживание
+            mPaint.setColor(Color.BLACK); //цвет черный
+            mPaint.setStrokeWidth(2f); //ширина контура 2 пикселя
+            mPaint.setStyle(Paint.Style.FILL); //закрашивать примитивы черным цветом
+            mPaint.setTextSize(50f); //размер шрифта для canvas.drawText()
+
+            canvas.drawText("Это только начало", 510, 710, mPaint); //рисует повернутый текст в точке (310, 1010)
+        }
+
+    }
+
+}
