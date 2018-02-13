588fb97de08ebbbe337088ee86418e265fbc1c8c
Дмитрий Виноградов <di.vinogradov@gmail.com>
2018-02-13 22:55:39 +0300

    Добавил поддержку Kotlin в проект. Добавил класс констант на Kotlin.

---
+++ asp/app/src/main/java/com/example/halconel/offtherails/Constants.kt
@@ -0,0 +1,22 @@
+package com.example.halconel.offtherails
+
+/**
+ * Created by halconel on 13.02.2018.
+ */
+//object Constants {
+//    var screenWigth : Int = 0
+//    var screenHieght : Int = 0
+//
+//    lateinit var dm : class(val Wigth : Int, val Hieght : Int)
+//}
+
+class Constants {
+    companion object {
+
+        @JvmField
+        var screenWight : Int = 0
+
+        @JvmField
+        var screenHeight : Int = 0
+    }
+}
--- asp/app/src/main/java/com/example/halconel/offtherails/MainActivity.java
+++ asp/app/src/main/java/com/example/halconel/offtherails/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.halconel.offtherails;
 
-        import android.app.Activity;
-        import android.os.Bundle;
-        import android.view.Window;
-        import android.view.WindowManager;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Установка полноэкранного режима
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Сохранение размера экрана в объект констант
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        Constants.screenWight = dm.widthPixels;
+        Constants.screenHeight = dm.heightPixels;
+
         setContentView(new GamePanel(this));
     }
-
 }
